4f9b877640680a17199e8938e194a6907ce0a5c8
Tejashri Mandlik <161927572+tejashri4@users.noreply.github.com>
2025-08-02 22:42:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function appendToDisplay(value)
+{
+    document.getElementById('display').value += value;
+
+}
+
+function clearDisplay()
+{
+    document.getElementById('display').value='';
+}
+
+function calculate()
+{
+    try{
+        const result=eval(document.getElementById('display').value);
+        document.getElementById('display').value=result;
+    }
+    catch{
+        alert("Invalid expression!");
+    }
+}
